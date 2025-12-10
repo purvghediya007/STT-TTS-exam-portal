15a8865f7be355a81af1073c4171f8f0f65c30b3
aryan210905 <aryan2109shah@gmail.com>
2025-12-10 11:17:05 +0530

    trying hermes mistral model 7b

--- backend/fastapi_backend/app/config.py
+++ backend/fastapi_backend/app/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
 
-    HF_EVAL_MODEL_NAME: str = "NousResearch/Hermes-2-Pro-Mistral-3B"
+    HF_EVAL_MODEL_NAME: str = "NousResearch/Hermes-2-Pro-Mistral-7B"
     STT_DEFAULT_MODEL: str = "whisper"
 
     class Config:
