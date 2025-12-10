406728a5e812ecb19d2b28fa1e180548e81a3cc0
aryan210905 <aryan2109shah@gmail.com>
2025-12-10 11:17:05 +0530

    trying hermes mistral model 7b

--- backend/fastapi_backend/app/main.py
+++ backend/fastapi_backend/app/main.py
@@ -15,7 +15,7 @@ async def lifespan(app: FastAPI):
     models.whisper_model = ModelGenerator.whisper_model_generator()
 
     # preload Qwen model
-    models.qwen_model = EvaluationEngine("NousResearch/Hermes-2-Pro-Mistral-3B")
+    models.qwen_model = EvaluationEngine("NousResearch/Hermes-2-Pro-Mistral-7B")
 
 
     # force Qwen download during startup
