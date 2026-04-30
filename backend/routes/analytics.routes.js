61951f8e48bdba0f3c1e38bd917a7f2baae8692c
purvghediya007 <ghediyapurv99@gmail.com>
2026-04-30 09:07:06 +0530

    reallow feature

--- backend/routes/analytics.routes.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-const analytics = require("../controllers/analytics.controller");
-
-router.get("/student/:studentId", analytics.getStudentAnalytics);
-
-module.exports = router;
